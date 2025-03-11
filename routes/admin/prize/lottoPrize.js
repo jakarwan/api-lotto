9baa157a46d4b0fdb774b41f07b35d121eaa78d3
jakarwan <jakarwan_159@hotmail.com>
2025-03-11 11:36:28 +0700

    fix

--- routes/admin/prize/lottoPrize.js
+++ routes/admin/prize/lottoPrize.js
@@ -170,7 +170,6 @@ WHERE p.date_lotto BETWEEN '${startDate}' AND '${endDate}'
 GROUP BY p.poy_code 
 ORDER BY MAX(lt.closing_time) DESC;
 `;
-console.log(sql,'sql')
           connection.query(
             sql,
             [startDate, endDate],
