b4a451941ebaa757ae457e5b6072c43c04f054e5
jakarwan <jakarwan_159@hotmail.com>
2025-03-12 15:59:32 +0700

    add function maxplay

--- routes/sql/lottoNumber.js
+++ routes/sql/lottoNumber.js
@@ -28,7 +28,32 @@ async function closeLottoNumberUpdate(updateField, params) {
 //     "INSERT INTO lotto_number (number, type_option, price, pay, discount, total, lotto_type_id, created_by, poy_code, status, date_lotto, installment_date) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, curdate(), ?)";
 //    var queryLotto = "SELECT number"
 // };
+
+async function maxPlayUpdate(params) {
+  try {
+    var sql = `UPDATE member SET max_play = ? WHERE id = ?`;
+    const rows = await query(sql, params);
+    return rows;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function updatePlayLimitMembers(params) {
+  try {
+    var sql = `UPDATE member SET max_play = ?`;
+    const rows = await query(sql, params);
+    return rows;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
 module.exports = {
   lottoNumberInsert,
   closeLottoNumberUpdate,
+  maxPlayUpdate,
+  updatePlayLimitMembers,
 };
